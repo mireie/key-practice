dfc2d087fb33510b595641c65d556285e036240d
mireie <michael.reiersgaard@gmail.com>
2021-07-06 10:40:33 -0700

    pass test for building ionian scale

--- __tests__/keys.test.js
+++ __tests__/keys.test.js
@@ -2,14 +2,35 @@ import Keys from '../src/js/keys.js'
 
 describe('Keys', () => {
 
-  test("should return the array position for the root note", () => {
-    let KeyMaster = new Keys();
-    expect(KeyMaster.ionianKey('F#')).toEqual(6);
+  test("should return the array position for the root note ", () => {
+    let inputKey = new Keys();
+    inputKey.indexKey('F#');
+    expect(inputKey.key).toEqual(6);
 
-  })
-  // test('should return scale for F# in ionian', () => {
-  //   let fSharp = new Keys();
-  //   fSharp.ionianKey('F#');
-  //   expect(fSharp.ionian).toEqual(["F#","G#","A#","B","C#","D#","F","F#"]);
-  // });
+  });
+
+  test('should add all modes as property of object for a given key', () => {
+    let fSharp = new Keys();
+    fSharp.indexKey("F#");
+    fSharp.getModes();
+    expect(fSharp.ionian).toEqual([6, 8, 10, 11, 13, 15, 17, 18]);
+    expect(fSharp.dorian).toEqual([6, 8, 9, 11, 13, 15, 17, 18]);
+    expect(fSharp.phrygian).toEqual([6, 8, 10, 11, 13, 15, 17, 18]);
+    expect(fSharp.lydian).toEqual([6, 8, 10, 11, 13, 15, 17, 18]);
+    expect(fSharp.mixolydian).toEqual([6, 8, 10, 11, 13, 15, 17, 18]);
+    expect(fSharp.aeolian).toEqual([6, 8, 9, 11, 13, 14, 16, 18]);
+    expect(fSharp.locrian).toEqual([6, 7, 9, 11, 12, 14, 16, 18]);
+  });
 });
+
+// fSharp.ChromaticScaleSharp(ionian array);
+//"F#","G#","A#","B","C#","D#","F","F#"
+
+/*
+array = [6, 8, 10, 11, 13, 15, 17, 18]
+const newIonian = array.map(function(element) {
+return this.ChromaticScaleSharp[element]
+});
+newIonian
+
+*/
--- src/js/keys.js
+++ src/js/keys.js
@@ -2,17 +2,54 @@ export default class Keys {
   constructor() {
     this.ChromaticScaleSharp = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B", "C"];
     this.ChromaticScaleFlat = ["C", "D♭", "D", "E♭", "E", "F", "G♭", "G", "A♭", "A", "B♭", "B", "C", "D♭", "D", "E♭", "E", "F", "G♭", "G", "A♭", "A", "B♭", "B", "C"];
-    this.ionian = "";
+    this.ionian = [];
+    this.dorian = [];
+    this.phrygian = [];
+    this.lydian = [];
+    this.mixolydian = [];
+    this.aeolian = [];
+    this.locrian = [];
+
+    this.key = "";
   }
 
-  ionianChange(i) {
-    return [i+0, i+2, i+4, i+5, i+7, i+9, i+11, i+12] //difference from root
+  indexKey(key) {
+    this.key = this.ChromaticScaleSharp.indexOf(key);
   }
 
-  ionianKey(key) {
-  return this.ChromaticScaleSharp.indexOf(key);
-    // const result = this.ionianChange(tempKey);
-    // this.ionian = result;
+  getModes() {
+    const i = this.key;
+    this.ionian =     [i + 0, i + 2, i + 4, i + 5, i + 7, i + 9, i + 11, i + 12];
+    this.dorian =     [i + 0, i + 2, i + 3, i + 5, i + 7, i + 9, i + 10, i + 12];
+    this.lydian =     [i + 0, i + 2, i + 4, i + 6, i + 7, i + 9, i + 11, i + 12];
+    this.phrygian =   [i + 0, i + 1, i + 3, i + 5, i + 7, i + 8, i + 10, i + 12];
+    this.mixolydian = [i + 0, i + 2, i + 4, i + 5, i + 7, i + 9, i + 10, i + 12];
+    this.aeolian =    [i + 0, i + 2, i + 3, i + 5, i + 7, i + 8, i + 10, i + 12];
+    this.locrian =    [i + 0, i + 1, i + 3, i + 5, i + 6, i + 8, i + 10, i + 12];
   }
+  
+  // sharpLoop() {
+  //   this.ionian = this.ionian.map(function(element) {
+  //     return this.ChromaticScaleSharp[element]
+  //   })
+  //   this.dorian = this.dorian.map(function(element) {
+  //     return this.ChromaticScaleSharp[element]
+  //   })
+  //   this.phrygian = this.phrygian.map(function(element) {
+  //     return this.ChromaticScaleSharp[element]
+  //   })
+  //   this.lydian = this.lydian.map(function(element) {
+  //     return this.ChromaticScaleSharp[element]
+  //   })
+  //   this.mixolydian = this.mixolydian.map(function(element) {
+  //     return this.ChromaticScaleSharp[element]
+  //   })
+  //   this.aeolian = this.aeolian.map(function(element) {
+  //     return this.ChromaticScaleSharp[element]
+  //   })
+  //   this.locrian = this.locrian.map(function(element) {
+  //     return this.ChromaticScaleSharp[element]
+  //   })
+  // }
 
 }
